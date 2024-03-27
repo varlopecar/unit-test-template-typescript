c58e016374db54ff0b06866310c60824e83fa40a
Rael Calitro <rael06@hotmail.fr>
2024-03-27 17:14:09 +0100

    first commit

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/src"],
+  testMatch: ["**/*.test.ts", "**/*.spec.ts"],
+};
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+async function start() {
+  console.log("Application starts...");
+
+  console.log("Application ends...");
+}
+
+start();
